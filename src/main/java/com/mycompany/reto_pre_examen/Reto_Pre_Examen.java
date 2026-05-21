78f60bbcc24f45635fa34b564cb3541882eb1e72
Olga Lucia Guacaneme Archila <est.olga.guacaneme@unimilitar.edu.co>
2026-05-21 10:25:33 -0500

    Se copio el codigo del reto

--- src/main/java/com/mycompany/reto_pre_examen/Reto_Pre_Examen.java
+++ src/main/java/com/mycompany/reto_pre_examen/Reto_Pre_Examen.java
@@ -1,9 +1,38 @@
 
 package com.mycompany.reto_pre_examen;
 
-public class Reto_Pre_Examen {
+//public class Reto_Pre_Examen {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+ import java.util.Scanner;
+
+class Lectura {
+  private static Scanner inp = new Scanner(System.in);
+  public static int nextInt(String mss) {
+    System.out.print(mss);
+    return inp.nextInt();
+  }
+}
+
+class FilaIndia 
+
+}
+
+public class SupervisionSoldados {
+	public static void main(String[] args) {
+		FilaIndia filaI = new FilaIndia();
+		int comandantes[], subordinados[];
+
+		int n = Lectura.nextInt("Cuántos soldados en la fila: ");
+		for (int i = 0; i < n; i++)
+		  filaI.add(Lectura.nextInt("Siguiente soldado: "));
+
+		int s = Lectura.nextInt("Cuántos supervisados: ");
+		comandantes = new int[s];
+		subordinados = new int[s];
+		for (int i = 0; i < s; i++) {
+		  comandantes[i] = Lectura.nextInt("Siguiente soldado supervisor: ");
+		  subordinados[i] = Lectura.nextInt("Siguiente soldado sobordinado: ");
+		}
+		filaI.revisar(comandantes, subordinados);
+	}
 }
