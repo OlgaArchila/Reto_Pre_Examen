c8529cdad6d794cfe1ddfaad022ede509b7e31d7
Olga Lucia Guacaneme Archila <est.olga.guacaneme@unimilitar.edu.co>
2026-05-21 10:42:32 -0500

    se crearon los arreglos de comandante y de soldado, el constructor para decir cuantos parametros y decir que al inicio no hay

--- src/main/java/com/mycompany/reto_pre_examen/Reto_Pre_Examen.java
+++ src/main/java/com/mycompany/reto_pre_examen/Reto_Pre_Examen.java
@@ -6,8 +6,7 @@ package com.mycompany.reto_pre_examen;
  import java.util.Scanner;// Importa Scanner para leer datos del teclado
 
 class Lectura {// Clase para manejar la lectura de datos
-  private static Scanner inp = new Scanner(System.in);// Objeto Scanner global
-  //error porque no es inp es int
+  private static Scanner inp = new Scanner(System.in);// 
   public static int nextInt(String mss) {// Método para leer enteros
     System.out.print(mss);// Muestra el mensaje en pantalla
     return inp.nextInt();// Retorna el número ingresado
@@ -15,7 +14,20 @@ class Lectura {// Clase para manejar la lectura de datos
 }
 // Clase que representa la fila de soldados
 class FilaIndia{ //falta completar el codigo
-}
+     private int fila[]; // Arreglo donde se guardan los soldados
+     private int cant; // Variable para controlar cuántos hay
+    
+     public FilaIndia() { // Constructor de la clase
+    fila = new int[100]; // Crea espacio para 100 soldados
+    cant = 0; // Inicialmente no hay soldados
+     }
+     
+    public void add(int soldado) { // Método para agregar soldados
+    fila[cant] = soldado; // Guarda el soldado en la posición actual
+    cant++; // Aumenta la cantidad de soldados
+  }
+  }
+
 
 public class SupervisionSoldados { // Clase principal
 	public static void main(String[] args) {// Método principal
